7985b6dd3409a83181f4996d4f58c0c812eb961a
Alex Tran <tranduclong092@gmail.com>
2024-09-24 11:52:39 +0300

    Chore: update config drupal url and session token

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   // copy this file to config.js and edit the settings to match yours
-  drupal_url: 'https://my-first-drupal9-app.lndo.site'
+  drupal_url: 'http://127.0.0.1:60488'
 }
